d4b82b68b82f2cb8a6f920cb786d6301d0dfea72
Mark Wetzel <mark@markwetzel.com>
2023-04-07 16:16:07 -0400

    Implement WeatherForecastController/add CORS config. .

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,32 +1,52 @@
+using API.Exceptions;
+using API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("/api/v1/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IWeatherService _weatherService;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(
+        ILogger<WeatherForecastController> logger,
+        IWeatherService weatherService
+    )
     {
         _logger = logger;
+        _weatherService = weatherService;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Get([FromQuery(Name = "city")] string[] cities)
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        try
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            var weatherData = await _weatherService.GetWeatherData(cities);
+            Response.Headers.Add("Cache-Control", $"public, max-age={3600}");
+            return new OkObjectResult(weatherData);
+        }
+        catch (MissingApiKeyException ex)
+        {
+            // Log the exception
+            _logger.LogError(ex, "Missing API key: {Message}", ex.Message);
+
+            // Return a user-friendly error message
+            return StatusCode(500, new { Error = "An internal server error occurred. Please contact the administrator." });
+        }
+        catch (Exception ex)
+        {
+            // Log the exception
+            _logger.LogError(ex, "An error occurred while fetching weather data: {Message}", ex.Message);
+
+            // Return a generic error message
+            return StatusCode(500, new { Error = "An error occurred while fetching weather data. Please try again later." });
+        }
     }
+
 }
--- API/Program.cs
+++ API/Program.cs
@@ -3,8 +3,24 @@ using Common.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowSpecificOrigins",
+        builder =>
+        {
+            builder
+                .WithOrigins("http://localhost:3000", "https://localhost:3000") // Replace these with the appropriate client origins
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        }
+    );
+});
 
+// Add services to the container.
+builder.Services.AddHttpClient();
+builder.Services.AddMemoryCache();
 builder.Services.AddControllers();
 builder.Services.Configure<WeatherApiOptions>(builder.Configuration.GetSection("WeatherApi"));
 
@@ -15,6 +31,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IWeatherService, WeatherService>();
 
 var app = builder.Build();
+
+// Add CORS middleware to the pipeline
+app.UseCors("AllowSpecificOrigins");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
