eb571f2f68cff30ae7a60e76c80a219d4afa2ffe
Mark Wetzel <mark@markwetzel.com>
2023-04-07 14:27:59 -0400

    Add strongly-typed config.

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,12 @@
+using Common.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.Configure<WeatherApiOptions>(builder.Configuration.GetSection("WeatherApi"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
