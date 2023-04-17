b7be0bfbb8bade60bb3a9ad4db8d2da85e279544
Mark Wetzel <mark@markwetzel.com>
2023-04-17 18:50:26 -0400

    WIP. Service (and docker-compose up) not working.

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,5 @@
-const apiBaseUrl = "https://localhost:5000/api/v1";
+const apiBaseUrl =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api/v1";
 
 export const forecastEndpoint = (cities: string[]) =>
   `${apiBaseUrl}/WeatherForecast?city=${cities.join("&city=")}`;
