b0cc62f0351d485e5d14332e4b809f93daf55368
Mark Wetzel <mark@markwetzel.com>
2023-04-21 17:44:20 -0400

    Add favicon.

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,5 +1,5 @@
 const apiBaseUrl =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api/v1";
+  process.env.REACT_APP_API_URL || "http://localhost:5000/api/v1";
 
 export const forecastEndpoint = (cities: string[]) =>
   `${apiBaseUrl}/WeatherForecast?city=${cities.join("&city=")}`;
